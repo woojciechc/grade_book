a563e4dd2c93c6497ed49e3eb06382dc8ae947ee
Wojciech Ciesielski <woojciechc@gmail.com>
2023-06-01 16:36:30 +0200

    style: style grades

--- app/Views/student/grades.php
+++ app/Views/student/grades.php
@@ -1,18 +1,22 @@
 <?= $this->extend('common/template') ?>
 <?= $this->section('content') ?>
-    <div class="container">
-        <div class="row justify-content-md-center">
-            <div class="col-12 m-3">
-                <?php foreach ($grades as $item): ?>
-                    <button data-toggle="tooltip" data-placement="bottom" title="<?php echo $item['description'] ?>"
-                        type="button" class="btn btn-<?php echo $item['color'] ?>">
-                        <?php echo $item['grade_value'] ?>
-                    </button>
-                <?php endforeach ?>
-            </div>
-
+<div class="container">
+<?php echo "<h1>" . $title ."</h1>" ?>
+    <div class="row justify-content-md-center">
+        <div class="col-12 m-3">
+            <?php if (empty($grades)) {
+                echo "Brak ocen";
+            } ?>
+            <?php foreach ($grades as $item): ?>
+                <button data-toggle="tooltip" data-placement="bottom" title="<?php echo $item['description'] ?>"
+                    type="button" class="btn btn-<?php echo $item['color'] ?>">
+                    <?php echo $item['grade_value'] ?>
+                </button>
+            <?php endforeach ?>
         </div>
+
     </div>
+</div>
 
 </html>
 
