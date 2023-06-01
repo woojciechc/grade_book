f211bd0a8f7a09c2a77864d1b7b1997f8f057b7b
Wojciech Ciesielski <woojciechc@gmail.com>
2023-06-01 16:30:12 +0200

    feature: add button in header

--- app/Controllers/UserManagementController.php
+++ app/Controllers/UserManagementController.php
@@ -1,9 +1,11 @@
-<?php namespace App\Controllers;
- 
+<?php
+namespace App\Controllers;
+
 use CodeIgniter\Controller;
 use App\Models\UserModel;
- 
-class UserManagementController extends Controller {
+
+class UserManagementController extends Controller
+{
     public function index()
     {
         helper(['form']);
@@ -22,38 +24,38 @@ class UserManagementController extends Controller {
         helper(['form']);
 
         $rules = [
-            'email'         => 'required|min_length[6]|max_length[50]|valid_email|is_unique[user.email]',
-            'firstName'     => 'required|min_length[3]|max_length[20]',
-            'lastName'      => 'required|min_length[3]|max_length[20]',
-            'password'      => 'required|min_length[6]|max_length[200]',
-            'role'          => 'required',
-            'confpassword'  => 'matches[password]'
+            'email' => 'required|min_length[6]|max_length[50]|valid_email|is_unique[user.email]',
+            'firstName' => 'required|min_length[3]|max_length[20]',
+            'lastName' => 'required|min_length[3]|max_length[20]',
+            'password' => 'required|min_length[6]|max_length[200]',
+            'role' => 'required',
+            'confpassword' => 'matches[password]'
         ];
-         
-        if($this->validate($rules)){
+
+        if ($this->validate($rules)) {
             $model = new UserModel();
             $data = [
-                'email'    => $this->request->getVar('email'),
-                'firstName'=> $this->request->getVar('firstName'),
+                'email' => $this->request->getVar('email'),
+                'firstName' => $this->request->getVar('firstName'),
                 'lastName' => $this->request->getVar('lastName'),
-                'role_id'  => $this->request->getVar('role'),
+                'role_id' => $this->request->getVar('role'),
                 'password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT)
             ];
             $model->save($data);
             return redirect()->to('/login');
-        }else{
+        } else {
             $data['validation'] = $this->validator;
             return view('registration', $data);
         }
-         
+
     }
- 
+
     public function login()
     {
         helper(['form']);
         return view('login');
-    } 
- 
+    }
+
     public function auth()
     {
         $session = session();
@@ -61,29 +63,37 @@ class UserManagementController extends Controller {
         $email = $this->request->getVar('email');
         $password = $this->request->getVar('password');
         $data = $model->where('email', $email)->first();
-        if($data){
+        if ($data) {
             $pass = $data['password'];
-            $hash = substr( $pass, 0, 60 );
+            $hash = substr($pass, 0, 60);
             $verify_pass = password_verify($password, $hash);
-            if($verify_pass){
+            if ($verify_pass) {
                 $ses_data = [
-                    'user_id'       => $data['id'],
-                    'user_email'    => $data['email'],
-                    'logged_in'     => TRUE,
-                    'roleId'        => $data['role_id']
+                    'user_id' => $data['id'],
+                    'user_email' => $data['email'],
+                    'logged_in' => TRUE,
+                    'roleId' => $data['role_id']
                 ];
                 $session->set($ses_data);
-                return redirect()->to('student/subjects');
-            }else{
+                switch ($data['role_id']) {
+                    case 3:
+                        return redirect()->to('student/subjects');
+                    case 2:
+                        return redirect()->to('teacher/classes');
+                    case 1:
+                        return redirect()->to('admin/dashboard');
+
+                }
+            } else {
                 $session->setFlashdata('msg', 'Błędne hasło');
                 return redirect()->to('/login');
             }
-        }else{
+        } else {
             $session->setFlashdata('msg', 'Nie znaleziono danych logowania');
             return redirect()->to('/login');
         }
     }
- 
+
     public function logout()
     {
         $session = session();
@@ -94,7 +104,7 @@ class UserManagementController extends Controller {
     public function dashboard()
     {
         $session = session();
-        echo "Welcome back, ".$session->get('user_email');
+        echo "Welcome back, " . $session->get('user_email');
     }
 
 }
